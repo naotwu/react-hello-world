e5c335273b352113c6dc33ebf19db50a4e1755b4
naotwu <naotwu@gmail.com>
2023-06-30 07:53:43 +0900

    feat: 型解決

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [tasks, setTasks] = useState([]);         // タスクのリストを管理するステート
-  const [newTask, setNewTask] = useState('');     // 新しいタスクの入力値を管理するステート
-  const [editId, setEditId] = useState(null);     // 編集中のタスクIDを管理するステート
-  const [editTitle, setEditTitle] = useState(''); // 編集中のタスクのタイトルを管理するステート
+
+  type Task = {
+    id: number;
+    title: string;
+    completed: boolean;
+  };
+  
+  const [tasks, setTasks] = useState<Task[]>([]);             // タスクのリストを管理するステート
+  const [newTask, setNewTask] = useState<string>('');         // 新しいタスクの入力値を管理するステート
+  const [editId, setEditId] = useState<number | null>(null);  // 編集中のタスクIDを管理するステート
+  const [editTitle, setEditTitle] = useState<string>('');     // 編集中のタスクのタイトルを管理するステート
 
   // 新しいタスクを追加する関数
   const addTask = () => {
@@ -16,7 +23,7 @@ function App() {
   };
 
   // タスクの完了状態を切り替える関数
-  const toggleTaskCompletion = (taskId) => {
+  const toggleTaskCompletion = (taskId: number) => {
     const updatedTasks = tasks.map((task) => {
       if (task.id === taskId) {
         return { ...task, completed: !task.completed };
@@ -27,13 +34,13 @@ function App() {
   };
 
   // タスクを削除する関数
-  const deleteTask = (taskId) => {
+  const deleteTask = (taskId: number) => {
     const updatedTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(updatedTasks);
   };
 
   // タスクの編集内容を更新する関数
-  const editTask = (taskId, newTitle) => {
+  const editTask = (taskId: number, newTitle: string) => {
     const updatedTasks = tasks.map((task) => {
       if (task.id === taskId) {
         return { ...task, title: newTitle };
@@ -43,14 +50,16 @@ function App() {
     setTasks(updatedTasks);
   };
 
-  const startEditTask = (taskId, currentTitle) => {
+  const startEditTask = (taskId: number, currentTitle: string) => {
     setEditId(taskId);
     setEditTitle(currentTitle);
   };
 
   const endEditTask = () => {
     if (editTitle.trim() !== '') {
-      editTask(editId, editTitle);
+      if (editId !== null) {
+        editTask(editId, editTitle);
+      }
     }
     setEditId(null);
     setEditTitle('');
